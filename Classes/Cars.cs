8fc214c43cd184f7315c00015355c4676f99ed1c
Katelyn Yaeger <knyaeger93@gmail.com>
2022-10-06 16:28:27 -0500

    initial commit

---
+++ Classes/Cars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Classes
+{
+    public class Cars
+    {
+
+        public Cars(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+        public Cars()
+        {
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var myCar = new Cars();
+            myCar.Make = "Honda";
+            myCar.Model = "Civic";
+            myCar.Year = 2003;
+
+            var Volkswagen = new Cars("Volkswagen", "Beetle", 2018);
+            var VW = new Cars("Volkswagen", "Cabriolet", 1986);
+
+            var carList = new List<Cars>() { myCar, Volkswagen, VW };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+
         }
+
+
+        
     }
 }
